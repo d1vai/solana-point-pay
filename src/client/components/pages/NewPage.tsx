f8e7958c1191b639260fa072750f6609406b7889
aboutmydreams <aboutmydreams@163.com>
2024-10-18 17:50:11 +0800

    fix: use custom pay ui

--- src/client/components/pages/NewPage.tsx
+++ src/client/components/pages/NewPage.tsx
@@ -12,38 +12,17 @@ import css from "./NewPage.module.css";
 const NewPage: NextPage = () => {
   const phone = useMediaQuery({ query: "(max-width: 767px)" });
 
-  return phone ? (
+  return (
     <div className={css.root}>
-      {/* <div className={css.top}>
+      <div className={css.top}>
         <FullscreenButton />
-        <TransactionsLink />
-      </div> */}
+        {phone && <TransactionsLink />} {/* 仅在移动端显示 */}
+      </div>
       <div className={css.body}>
         <NumPad />
         <GenerateButton />
       </div>
       <PoweredBy />
-    </div>
-  ) : (
-    <div className={css.root}>
-      <div className={css.main}>
-        <div className={css.top}>
-          <FullscreenButton />
-        </div>
-        <div className={css.body}>
-          <NumPad />
-        </div>
-        <PoweredBy />
-      </div>
-      <div className={css.side}>
-        <div className={css.summary}>
-          <Summary />
-          <GenerateButton />
-        </div>
-        <div className={css.bottom}>
-          <TransactionsLink />
-        </div>
-      </div>
     </div>
   );
 };
